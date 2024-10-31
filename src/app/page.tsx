0ab5cff83a15e42d7bd52329fde0603fe94a5fcc
kaziabulhasib <hasibkazi420@gmail.com>
2024-10-31 18:04:52 +0530

    navigation link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Home</h1>
+      <button className='border-red-500 border-2 rounded-lg px-6 py-2 font-medium my-6'>
+        <Link href='products'>Products</Link>
+      </button>
     </div>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,35 @@
-export const metadata = {
-  title: "Products Page",
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Products",
+  },
 };
 
 export default function Products() {
   return (
-    <>
-      <h1>Products</h1>
-      <h2>Product1</h2>
-      <h2>Product2</h2>
-      <h2>Product3</h2>
-    </>
+    <div className='text-center w-1/2 mx-auto border mt-24 py-8'>
+      <h1 className='text-3xl'>Products</h1>
+      <h2>
+        <Link className='underline my-6' href='products/1'>
+          Product1
+        </Link>
+      </h2>
+      <h2>
+        <Link className='underline my-6' href='products/2'>
+          Product2
+        </Link>
+      </h2>
+      <h2>
+        <Link className='underline my-6' href='products/3'>
+          Product3
+        </Link>
+      </h2>
+
+      <button className='border-red-500 border-2 rounded-lg px-6 py-2 font-medium my-6'>
+        <Link href='/'>Home</Link>
+      </button>
+    </div>
   );
 }
