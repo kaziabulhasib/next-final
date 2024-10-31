75528a3b5499a8c441bf6faf3ce28007eac8adec
kaziabulhasib <hasibkazi420@gmail.com>
2024-10-31 22:06:49 +0530

    router from next ne methods

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,22 @@
 "use client";
-import Link from "next/link";
+
 import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function NotFound() {
   const router = useRouter();
-  const handleClick = () => {
-    // alert("bakc page");
-    router.back();
-  };
+
   return (
     <div className='text-center my-8 space-y-6'>
       <h1 className='text-3xl text-red-500'>Not found</h1>
       <p>Page is not found, back to home</p>
-      <button className='border-red-500 border-2 rounded-lg px-6 py-2 font-medium'>
-        <Link href='/'>Home</Link>
+      <button
+        className='border-red-500 border-2 rounded-lg px-6 py-2 font-medium'
+        onClick={() => router.push("/")}>
+        Home
       </button>
       <button
-        onClick={handleClick}
+        onClick={() => router.back()}
         className='border-red-500 border-2 rounded-lg px-6 py-2 font-medium'>
         Prev Page
       </button>
